4327ea3ccee68bda5f67f30ad33b7c4fbf9e967c
michaeld96 <michaeldick000@gmail.com>
2022-07-13 22:21:41 -0400

    added sdl code.

--- Game-Loop-Imp/main.c
+++ Game-Loop-Imp/main.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include "constants.h"
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+int initialize_window(void)
+{
+    // If SDL doesn't return 0, then something didn't 
+    // happen properly.
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return FALSE;
+    }
+    // SDL_Window(title, xpos, ypos, width, height, boarder)
+    window = SDL_CreateWindow(
+        NULL, 
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SDL_WINDOW_BORDERLESS
+        );
+
+    if(!window)
+    {
+        fprintf(stderr, "Error creating SDL window.\n");
+        return FALSE;
+    }
+    //SDL_CreateRenderer(*window, display_number, render)
+    // -1 means default driver.
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if(!renderer)
+    {
+        fprintf(stderr, "Error creating SDL Renderer.\n");
+        return FALSE;
+    }
+    // Everything is OK.
+    return TRUE;
+}
+
 
 int main()
 {
-    printf("Game is Running \n");
+    initialize_window();
     return 0;
 }
